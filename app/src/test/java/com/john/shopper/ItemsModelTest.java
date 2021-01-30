6ff69bdd2e3342ed2d5a3ad3488b7ea2b23aabac
johneastman <jp.robotics@gmail.com>
2021-01-30 12:06:21 -0600

    add tests for ItemsModel.getNumberOfIncompleteItems

--- app/src/test/java/com/john/shopper/ItemsModelTest.java
+++ app/src/test/java/com/john/shopper/ItemsModelTest.java
@@ -48,4 +48,60 @@ public class ItemsModelTest {
 
         assertEquals(expectedPosition, actualPosition);
     }
+
+    @Test
+    public void testgetNumberOfIncompleteItemsWhenAllItemsIncomplete()
+    {
+        ItemsModel.getInstance().addItem(0, "Section Header", 1, true);
+        ItemsModel.getInstance().addItem(1, "Item 1.1", 1, false);
+        ItemsModel.getInstance().addItem(2, "Item 1.2", 2, false);
+        ItemsModel.getInstance().addItem(3, "Section 2", 1, true);
+        ItemsModel.getInstance().addItem(4, "Item 2.1", 1, false);
+        ItemsModel.getInstance().addItem(5, "Item 2.2", 3, false);
+
+        int actualNumberOfInCompleteItems = ItemsModel.getInstance().getNumberOfIncompleteItems();
+        int expectedNumberOfIncompleteItems = 7;
+
+        assertEquals(expectedNumberOfIncompleteItems, actualNumberOfInCompleteItems);
+    }
+
+    @Test
+    public void testgetNumberOfIncompleteItemsWhenSomeItemsComplete()
+    {
+        ItemsModel.getInstance().addItem(0, "Section Header", 1, true);
+        ItemsModel.getInstance().addItem(1, "Item 1.1", 1, false);
+        ItemsModel.getInstance().addItem(2, "Item 1.2", 2, false);
+        ItemsModel.getInstance().addItem(3, "Section 2", 1, true);
+        ItemsModel.getInstance().addItem(4, "Item 2.1", 1, false);
+        ItemsModel.getInstance().addItem(5, "Item 2.2", 3, false);
+
+        ItemsModel.getInstance().get(1).setComplete(true);
+        ItemsModel.getInstance().get(5).setComplete(true);
+
+        int actualNumberOfInCompleteItems = ItemsModel.getInstance().getNumberOfIncompleteItems();
+        int expectedNumberOfIncompleteItems = 3;
+
+        assertEquals(expectedNumberOfIncompleteItems, actualNumberOfInCompleteItems);
+    }
+
+    @Test
+    public void testgetNumberOfIncompleteItemsWhenAllItemsComplete()
+    {
+        ItemsModel.getInstance().addItem(0, "Section Header", 1, true);
+        ItemsModel.getInstance().addItem(1, "Item 1.1", 1, false);
+        ItemsModel.getInstance().addItem(2, "Item 1.2", 2, false);
+        ItemsModel.getInstance().addItem(3, "Section 2", 1, true);
+        ItemsModel.getInstance().addItem(4, "Item 2.1", 1, false);
+        ItemsModel.getInstance().addItem(5, "Item 2.2", 3, false);
+
+        ItemsModel.getInstance().get(1).setComplete(true);
+        ItemsModel.getInstance().get(2).setComplete(true);
+        ItemsModel.getInstance().get(4).setComplete(true);
+        ItemsModel.getInstance().get(5).setComplete(true);
+
+        int actualNumberOfInCompleteItems = ItemsModel.getInstance().getNumberOfIncompleteItems();
+        int expectedNumberOfIncompleteItems = 0;
+
+        assertEquals(expectedNumberOfIncompleteItems, actualNumberOfInCompleteItems);
+    }
 }
