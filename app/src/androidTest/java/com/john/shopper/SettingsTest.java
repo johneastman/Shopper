7e20bf5dbe441e8b015e5eacf0c6210bb1f55d59
johneastman <jp.robotics@gmail.com>
2022-03-28 10:30:31 -0600

    unit tests for developer mode

---
+++ app/src/androidTest/java/com/john/shopper/SettingsTest.java
@@ -0,0 +1,87 @@
+package com.john.shopper;
+
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import androidx.test.espresso.matcher.PreferenceMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.john.shopper.model.SettingsModel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SettingsTest extends UITestHelper {
+
+    Instrumentation inst = InstrumentationRegistry.getInstrumentation();
+    Context context = inst.getTargetContext();
+    SettingsModel settingsModel = new SettingsModel(context);
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setup() {
+        this.cleanup();
+    }
+
+    @After
+    public void teardown() {
+        this.cleanup();
+    }
+
+    public void cleanup() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+    @Test
+    public void testEnablingDeveloperMode() {
+        // Open the settings activity
+        performClickOnOverflowMenuItem(context, R.string.settings_menu);
+
+        // Click on Developer Mode checkbox to enable
+        performClickWithId(R.id.developer_mode_check_box);
+
+        // Assert that developer mode is enabled
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean isDeveloperModeEnabled = sharedPreferences.getBoolean(
+                SettingsActivity.DEVELOPER_MODE_KEY,
+                false);
+        assertTrue(isDeveloperModeEnabled);
+    }
+
+    @Test
+    public void testDisablingDeveloperMode() {
+        // Open the settings activity
+        performClickOnOverflowMenuItem(context, R.string.settings_menu);
+
+        // Click on Developer Mode checkbox twice to disable the setting. The first time ensures the developer mode setting
+        // is created and stored in the shared preferences. The second time disabled developer mode.
+        performClickWithId(R.id.developer_mode_check_box);
+        performClickWithId(R.id.developer_mode_check_box);
+
+        // Assert that developer mode is not enabled
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean isDeveloperModeEnabled = sharedPreferences.getBoolean(
+                SettingsActivity.DEVELOPER_MODE_KEY,
+                false);
+        assertFalse(isDeveloperModeEnabled);
+    }
+}
